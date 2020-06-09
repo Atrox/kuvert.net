44fc93c4475a695dd6be0e829aa48879056ffab4
Atrox <hello@atrox.dev>
2020-06-09 21:58:20 +0200

    add di test

---
+++ Kuvert.Tests/ServiceCollectionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Kuvert.Models;
+using Kuvert.Templates;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Kuvert.Tests
+{
+    public class ServiceCollectionTest
+    {
+        [Fact]
+        public void TestKuvertServiceCollection()
+        {
+            var services = new ServiceCollection();
+
+            // require configuration
+            Assert.Throws<ArgumentException>(() => { services.AddKuvert(options => { }); });
+
+            // add kuvert
+            services.AddKuvert(options =>
+            {
+                options.Product = new Product("test product", "testlink");
+            }).AddDefaultTemplate();
+            
+            var provider = services.BuildServiceProvider();
+            
+            // kuvert service
+            var kuvertService = provider.GetRequiredService<IKuvert>();
+            Assert.IsType<KuvertService>(kuvertService);
+
+            // template service
+            var templateService = provider.GetRequiredService<IKuvertTemplate>();
+            Assert.IsType<KuvertDefaultTemplate>(templateService);
+            
+            // renderer service
+            var rendererService = provider.GetRequiredService<IKuvertRenderer>();
+            Assert.IsType<RazorRenderer>(rendererService);
+        }
+    }
+}
