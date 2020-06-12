06922efbaa71d6fb79c94818d6cef9168448030d
Atrox <hello@atrox.dev>
2020-06-12 19:51:49 +0200

    try to fix weird razorlight errors when using in docker container

--- Kuvert/RazorRenderer.cs
+++ Kuvert/RazorRenderer.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using RazorLight;
+using RazorLight.Razor;
 
 namespace Kuvert
 {
@@ -10,7 +12,7 @@ namespace Kuvert
         public RazorRenderer()
         {
             _engine = new RazorLightEngineBuilder()
-                .UseEmbeddedResourcesProject(typeof(IKuvert))
+                .UseProject(new InMemoryRazorLightProject())
                 .UseMemoryCachingProvider()
                 .Build();
         }
@@ -18,6 +20,19 @@ namespace Kuvert
         public async Task<string> ParseAsync<T>(string key, string template, T model)
         {
             return await _engine.CompileRenderStringAsync(key, template, model);
+        }
+    }
+
+    public class InMemoryRazorLightProject : RazorLightProject
+    {
+        public override Task<RazorLightProjectItem> GetItemAsync(string templateKey)
+        {
+            return Task.FromResult<RazorLightProjectItem>(new TextSourceRazorProjectItem(templateKey, templateKey));
+        }
+
+        public override Task<IEnumerable<RazorLightProjectItem>> GetImportsAsync(string templateKey)
+        {
+            return Task.FromResult<IEnumerable<RazorLightProjectItem>>(new List<RazorLightProjectItem>());
         }
     }
 }
